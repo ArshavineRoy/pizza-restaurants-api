e00132357b6531af38d1779caa80ba45748fd5dd
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-24 19:43:45 +0300

    add home route to app

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 from models import db, Restaurant, Pizza, RestaurantPizza
 
@@ -15,6 +15,23 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the RestaurantPizza RESTful API"
+        }
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+api.add_resource(Home, '/')
+
+
+
+
 
 
 if __name__ == '__main__':
