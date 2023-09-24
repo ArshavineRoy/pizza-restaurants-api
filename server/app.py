96af983aecb391c79e886629cdfb7579bb950eb5
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-24 18:00:33 +0300

    init and perform db migrations to create tables

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 
-from models import db, Plant
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
