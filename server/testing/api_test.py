fb8381a22db043ef6aaeb2bca5fb56d697777ca0
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-25 21:02:39 +0300

    add DELETE /restaurants/:id test

--- server/testing/api_test.py
+++ server/testing/api_test.py
@@ -2,7 +2,7 @@ import json
 from app import app
 from models import db, Restaurant, Pizza, RestaurantPizza
 
-class TestPlant:
+class TestFlaskAPI:
     '''Flask RESTful API'''
 
     def test_restaurants_endpoint(self):
@@ -11,7 +11,7 @@ class TestPlant:
         assert(response.status_code == 200)
 
     def test_restaurants_endpoint_returns_json(self):
-        '''provides a response content type of application/json at "/restaurants"'''
+        '''provides a response content type of application/json at "/restaurants".'''
         response = app.test_client().get('/restaurants')
         assert response.content_type == 'application/json'
 
@@ -35,12 +35,12 @@ class TestPlant:
             db.session.commit()
 
     def test_get_restaurant_by_id(self):
-        '''has a resource available at "/restaurants/<int:id>".'''
+        '''has a resource available at "/restaurants/:id".'''
         response = app.test_client().get('/restaurants/1')
         assert(response.status_code == 200)
 
     def test_get_restaurant_by_id_returns_one_restaurant(self):
-        '''returns JSON representing one Restaurant object at "/restaurants/<int:id>".'''
+        '''returns JSON representing one Restaurant object at "/restaurants/:id".'''
         response = app.test_client().get('/restaurants/1')
         data = json.loads(response.data.decode())
 
@@ -49,3 +49,19 @@ class TestPlant:
         assert(data["name"])
         assert(data["address"])
         assert(data["pizzas"])
+
+    def test_delete_restaurant_by_id_deletes_restaurant(self):
+        '''can DELETE restaurants through "/restaurants/:id".'''
+        with app.app_context():
+            vk = Restaurant(
+                name="Villa Rosa Kempinski", 
+                address="4801 Charles Falls, Nairobi"
+            )
+
+            db.session.add(vk)
+            db.session.commit()
+            
+            response = app.test_client().delete(f'/restaurants/{vk.id}')
+            data = response.data.decode()
+
+            assert(not data)
