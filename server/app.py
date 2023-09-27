eb0918006c41c17161425e7c573415f3fdbfc0c4
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-27 14:37:28 +0300

    fix: POST errors. Use ns.payload, not request.form

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from flask_restx import Api, Resource, Namespace
+from flask_restx import Api, Resource, Namespace, fields
 
 from models import db, Restaurant, Pizza, RestaurantPizza
 
@@ -58,6 +58,16 @@ class RestaurantPizzaSchema(ma.SQLAlchemySchema):
     price = ma.auto_field()
 
 restaurant_pizza_schema = RestaurantPizzaSchema()
+
+
+# restx swagger input
+restaurant_pizza_model = api.model(
+    "RestaurantPizza Input", {
+        "price": fields.Integer,
+        "restaurant_id": fields.Integer,
+        "pizza_id": fields.Integer,
+    }
+)
 
 @ns.route("/home")
 class Home(Resource):
@@ -144,7 +154,7 @@ class RestaurantByID(Resource):
         
         if not restaurant:
             response_body = {
-                "error": "Restaurant not found"
+                "error": "Restaurant not found."
             }
 
             response = make_response(
@@ -195,16 +205,17 @@ class Pizzas(Resource):
 @ns.route("/restaurant_pizzas")
 class RestaurantPizzas(Resource):
 
-
+    @ns.expect(restaurant_pizza_model)
     def post(self):
         restaurant_pizza = RestaurantPizza(
-            price=int(request.form["price"]),
-            restaurant_id=int(request.form["restaurant_id"]),
-            pizza_id=int(request.form["pizza_id"])
+            price=ns.payload["price"],
+            restaurant_id=ns.payload["restaurant_id"],
+            pizza_id=ns.payload["pizza_id"]
         )
+        # price = ns.payload["price"]
+        restaurant = Restaurant.query.filter(Restaurant.id == int(ns.payload["restaurant_id"])).first()
+        pizza = Pizza.query.filter(Pizza.id == int(ns.payload["pizza_id"])).first()
 
-        restaurant = Restaurant.query.filter(Restaurant.id == int(request.form["restaurant_id"])).first()
-        pizza = Pizza.query.filter(Pizza.id == int(request.form["pizza_id"])).first()
 
         if not pizza and not restaurant:
             return make_response(
@@ -220,6 +231,14 @@ class RestaurantPizzas(Resource):
             return make_response(
                 {"error": "Pizza not found."},
                 404
+            )
+        elif 1 < ns.payload["price"] > 30:
+            return make_response(
+                {
+                    "error": "Validation Error",
+                    "message": "Price must be between 1 and 30"
+                },
+                422
             )
         else:
             db.session.add(restaurant_pizza)
